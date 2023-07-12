8a71a71ee211e12272bbde40ec55a39fd7c86d32
Kunal Khare <khareji594@gmail.com>
2023-07-12 17:26:33 +0530

    Practise GSAP

---
+++ script.js
@@ -0,0 +1,21 @@
+var t1=gsap.timeline()
+t1.from("#textdiv h1",{
+    y:100,
+    opacity:0,
+    stagger:0.3
+})
+t1.from("#vertical h1",{
+    x:100,
+    opacity:0,
+    stagger:0.3
+})
+t1.from("#smallimg",{
+    y:100,
+    opacity:0,
+    stagger:0.3
+})
+t1.from("#part2",{
+    x:100,
+    opacity:0,
+    stagger:0.3
+})
